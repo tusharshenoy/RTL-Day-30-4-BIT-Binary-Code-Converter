e50d53f829f4e696d907c5db39c31ab5851dada3
T Tushar Shenoy <107348474+tusharshenoy@users.noreply.github.com>
2023-10-22 12:33:47 +0530

    Create IC7408.v

---
+++ IC7408.v
@@ -0,0 +1,11 @@
+//Verilog Code for AND gate IC IC7408
+module IC7408(P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13,P14);
+input P1,P2,P4,P5,P9,P10,P12,P13;
+input P7,P14;//SUPPLY
+output P3,P6,P8,P11;
+
+assign P3=P1&P2;
+assign P6=P4&P5;
+assign P8=P9&P10;
+assign P11=P12&P13;
+endmodule
